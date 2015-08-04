6685822a73de619e45c52689e416c9fdca7cbcdc
Justin <justinyates314@live.com>
2015-08-04 09:52:59 -0400

    Shortened the valid directory check

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 require "lfs"
 local curDir = lfs.currentdir()
 local b = [[\]]
-local function checkValidDir(dir) if dir == "." or dir == ".." or dir == "main.lua" then return false else return true end end
+local function checkValidDir(dir) return dir ~= "." and dir ~= ".." and dir ~= "main.lua" end
 local function scanDir(dir)
 	for fl in lfs.dir(dir) do
-		if checkValidDir(fl) == true then
+		if checkValidDir(fl) then
 			if lfs.attributes(dir..b..fl, "mode") == "directory" then
 				scanDir(dir..b..fl)
 				lfs.rmdir(dir..b..fl)
